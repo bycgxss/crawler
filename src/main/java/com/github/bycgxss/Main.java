2ad6dc2cfa2ab831629bdf6b4d82f3306d49f280
bycgxss <bycgxss@gmail.com>
2022-04-08 10:35:28 +0800

    initiailize project

---
+++ src/main/java/com/github/bycgxss/Main.java
@@ -0,0 +1,6 @@
+package com.github.bycgxss;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/github/bycgxss/SmokeTest.java
@@ -0,0 +1,8 @@
+package com.github.bycgxss;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void Test() {}
+}
